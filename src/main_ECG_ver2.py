19149a208ece438e8ba5283bcaa21c62eb2b7d27
rolopu1 <718694@unizar.es>
2026-03-29 19:57:30 +0200

    models functions

--- src/main_ECG_ver2.py
+++ src/main_ECG_ver2.py
@@ -5,6 +5,8 @@ from .lib.pan_tompkins import pan_tompkin
 from .lib.compute_hrv_hrf import compute_HRV_HRF
 from .lib.interpolate_NN import interpolate_NN_pchip
 from .lib.remove_ectopic_beat import remove_ectopic_beats
+
+
 def ECGprocessing(ecg_signal, fs, patient_id):
 
     all_results = pd.DataFrame()
