466a5ced1cd73e7795d05775330c72dbafb2d5d9
Matthew Reyna <matthew.a.reyna@emory.edu>
2026-06-03 16:39:13 -0400

    Update gap

--- evaluate_model.py
+++ evaluate_model.py
@@ -266,12 +266,12 @@ def evaluate_model(labels_files, predictions_files, prevalence_files):
         age = df_prevalence.loc[patient, id_age]
         prevalence_ages[i] = age
 
-    age_to_prevalence = compute_prevalence(ages, prevalence_labels, prevalence_ages, gap=2)
+    age_to_prevalence = compute_prevalence(ages, prevalence_labels, prevalence_ages, gap=1)
 
     # Evaluate the predictions.
     reward = compute_reward(labels, binary_predictions, ages, age_to_prevalence)
-    auroc_age = compute_auroc_age(labels, probability_predictions, ages, gap=2)
-    auroc_weighted = compute_auroc_weighted(labels, probability_predictions, ages, gap=2)
+    auroc_age = compute_auroc_age(labels, probability_predictions, ages, gap=1)
+    auroc_weighted = compute_auroc_weighted(labels, probability_predictions, ages, gap=1)
     auroc = compute_auroc(labels, probability_predictions)
     auprc = compute_auprc(labels, probability_predictions)
     accuracy = compute_accuracy(labels, binary_predictions)
